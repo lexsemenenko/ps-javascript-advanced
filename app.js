2c3d57c4d2d02da1b8ec7c5f1707090a49ef07ee
lexsemenenko <lex@lex.me>
2023-04-22 20:26:37 -0400

    Задание - Даты и время

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('test');
+function validateAge(dateString) {
+  // Переводим строку в дату
+  const birthDate = new Date(dateString);
+
+  // Получаем разницу в миллисекундах между текущей датой и датой рождения
+  const ageDifferenceMs = Date.now() - birthDate.getTime();
+
+  // Пробразуем миллисекунды в годы
+  // Делим на миллисекунды / секунды / минуты / часы / дни
+  let ageDate = ageDifferenceMs / 1000 / 60 / 60 / 24 / 365;
+
+  // Округляем полученное число в меньшую сторону, чтобы получить целое число лет
+  const age = Math.floor(ageDate);
+
+  // Возвращаем ответ если возраст больше либо равен 14
+  return age >= 14;
+}
+
+const age = validateAge('2009-01-01');
+
+console.log(age); // true
